6dbcc2011994523fdfed51a0b39b1ccf43de7bd5
Ed Peters <ed.peters@opower.com>
2015-02-25 12:16:31 -0700

    Creating skeleton for sub-modules

---
+++ src/mesh/stl_file.rs
@@ -0,0 +1,5 @@
+// This is where code for STL file handling should go
+
+// This guy depends on multiple sibling sub-modules, so he can use that here.
+use vector::Foo;
+use stl_mesh::Bar;
---
+++ src/mesh/stl_mesh.rs
@@ -0,0 +1,6 @@
+// This is where code for the STL mesh should go
+
+// This guy depends on a sibling sub-module, so he can use that here.
+use vector::Foo;
+
+pub struct Bar;
