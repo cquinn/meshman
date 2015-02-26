0dd829a1dded0da18d821e9a10a75247ad52831b
Justin Ryan <jryan@netflix.com>
2015-02-25 19:59:19 -0700

    Engine to run commands

--- src/main.rs
+++ src/main.rs
@@ -48,31 +48,22 @@ fn main() {
         Err(e) => { println!("STL file error: {}", e); return; }
     };
 
-    let mesh = file.as_mesh();
-
     file.println_debug();
     println!("");
 
     let mesh = file.as_mesh();
-    println!("Mesh: {:?}", &mesh);
 
     // Process free as commands
     let mut commands: Vec<Box<MeshOperation>> = Vec::new();
-    let mut free = matches.free;
+    let mut iter = matches.free.iter();
     loop {
-        let command_name = match free.pop() {
+        let command_name = match iter.next() {
             None => break, // empty
-            Some(x) => x,
+            Some(x) => x.clone(),
         };
-        let vector = match free.pop() {
+        let vector = match iter.next() {
             None => panic!("Every command requires a vector"),
-            Some(x) => {
-                let parts: Vec<f32> = x.split(',').filter_map(|s| s.parse::<f32>().ok() ).collect();
-                if (parts.len() != 3) {
-                    panic!("Vector must have three elements")
-                };
-                Vector3D{x: parts[0], y: parts[1], z: parts[2]}
-            },
+            Some(y) => arg_to_vector(y.clone()),
         };
         let command = match command_name.as_slice() {
             "rotate" => Box::new(RotateOperation { v: vector }),
@@ -81,11 +72,26 @@ fn main() {
         commands.push( command );
     }
 
+    // Engine
+    let mut it = commands.iter();
+    let mut changed_mesh = mesh;
+    loop {
+        match it.next() {
+            Some(command) => {
+                changed_mesh = command.apply(changed_mesh);
+            }
+            None => { break }
+        }
+    }
+
+    println!("Mesh: {:?}", &changed_mesh);
+
     // Do we open the file, if it doesn't exist yet?
     let generated_file = match File::open(&Path::new(output_file)) {
         Ok(f) => f,
         Err(e) => panic!("file error: {}", e),
     };
+
 }
 
 // Command pattern
@@ -101,6 +107,22 @@ impl MeshOperation for RotateOperation {
     fn apply(&self, mesh: Mesh) -> Mesh {
         return Mesh::new();
     }
+}
+
+fn arg_to_vector(arg: String) -> Vector3D {
+    let parts: Vec<f32> = arg.split(',').filter_map(|s| s.parse::<f32>().ok() ).collect();
+    if (parts.len() != 3) {
+        panic!("Vector must have three elements: {}", arg)
+    };
+    Vector3D{x: parts[0], y: parts[1], z: parts[2]}
+}
+
+#[test]
+fn converts_arg_to_vector() {
+    let result = arg_to_vector("1,2,3".to_string());
+    assert_eq!(result.x, 1.0);
+    assert_eq!(result.y, 2.0);
+    assert_eq!(result.z, 3.0);
 }
 
 fn print_usage(program: &str, opts: Options) {
